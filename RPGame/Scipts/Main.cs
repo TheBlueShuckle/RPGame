3fb51952a2aad404765a8429e48de7b2d58696a2
Ville Halldin <villehalldin05@gmail.com>
2023-05-15 20:36:21 +0200

    Fixed the bug with the player getting stuck into walls. Forgot to change velocity to zero after checking if the pos + the velocity was intersecting a tile

--- RPGame/Scipts/Main.cs
+++ RPGame/Scipts/Main.cs
@@ -34,7 +34,7 @@ namespace RPGame.Scipts
         {            
             graphics.PreferredBackBufferWidth = GraphicsDevice.DisplayMode.Width;
             graphics.PreferredBackBufferHeight = GraphicsDevice.DisplayMode.Height;
-            graphics.IsFullScreen = true;
+            graphics.IsFullScreen = false;
             graphics.ApplyChanges();
 
             base.Initialize();
